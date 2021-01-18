52d75c38de33b0dffcce260acebe73eec65d618d
Nandiyta <61815366+Nandiyta@users.noreply.github.com>
2021-01-18 23:48:50 +0700

    Fifth Commit

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ Class Hello{
 		System.out.printin("Tittle");
 		System.out.printin("Nanday: Aplikasi Java 2020");
 		System.out.printin("--- *** ---");
-		System.out.printin("END");
+		System.out.printin("SELESAI");
 		}
 }
