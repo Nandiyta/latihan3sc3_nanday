de5144f8cf4d837320e57dca189635b425234048
Nanda Yunita <yunitananda20@gmail.com>
2021-01-18 23:39:27 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 Class Hello{
 		public static void main (String[] args){
-		System.out.printin("Judul");
+		System.out.printin("Judul Projek");
 		System.out.printin("Nanday: Aplikasi Java 2020");
 		System.out.printin("--- *** ---");
 		}
