357bc31cf67d5123b705a4580f4d42290c3027f4
dinidamy <dinidamayanti3029@gmail.com>
2021-01-19 19:50:01 +0700

    Sixth Commit

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 Class Hello{
 		public static void main (String[] args){
 		System.out.printin("Tittle");
-		System.out.printin("Nanday: Aplikasi Java 2020");
+		System.out.printin("Nanda dan Dini: Aplikasi Java 2021");
 		System.out.printin("--- *** ---");
 		System.out.printin("SELESAI");
+		System.out,printin("User 2")
 		}
 }
